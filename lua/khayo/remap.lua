3d6fa83078ab1118dd8354a43695081d4d312987
khayo.rodrigues <khayovannucci@gmail.com>
2024-08-12 23:35:18 -0300

    compiler funcionando

--- lua/khayo/remap.lua
+++ lua/khayo/remap.lua
@@ -52,3 +52,15 @@ vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>")
 
 -- abre o file explorer
 vim.keymap.set('n', '<leader>e' ,"<cmd>NvimTreeToggle<CR>")
+
+-- Open compiler
+vim.api.nvim_set_keymap('n', '<F6>', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+
+-- Redo last selected option
+vim.api.nvim_set_keymap('n', '<S-F6>',
+     "<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
+  .. "<cmd>CompilerRedo<cr>",
+ { noremap = true, silent = true })
+
+-- Toggle compiler results
+vim.api.nvim_set_keymap('n', '<S-F7>', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -79,5 +79,24 @@ return {
     },
     {
         "Hoffs/omnisharp-extended-lsp.nvim"
-    }
+    },
+    { -- This plugin
+      "Zeioth/compiler.nvim",
+      cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
+      dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+      opts = {},
+    },
+    { -- The task runner we use
+      "stevearc/overseer.nvim",
+      commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+      cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+      opts = {
+        task_list = {
+          direction = "bottom",
+          min_height = 5,
+          max_height = 5,
+          default_detail = 1
+        },
+      },
+    },
 }
