7c8934ff7071888be07bf09ba1ba628f20bb8a0c
khayo.rodrigues <khayovannucci@gmail.com>
2024-08-19 23:12:42 -0300

    Resolução de dependencias da forma recomendada (lsp-zero)
    Com a migração do lsp zero para a versão 4, a recomendação de como fazer
    a resolução de dependencias mudou, adequei o código para atender
    recomendação

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,17 +37,12 @@ return {
     {
         'mbbill/undotree'
     },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v4.x',
-        dependencies = {
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-            { 'neovim/nvim-lspconfig' },
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-        }
-    },
+    { 'VonHeikemen/lsp-zero.nvim', branch = 'v4.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
     {
         'L3MON4D3/LuaSnip',
         version = 'v2.*',
