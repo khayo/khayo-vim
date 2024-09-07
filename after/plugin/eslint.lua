d3836da202d24f64683e9cccc8d87ff59ef8acc0
khayo.rodrigues <khayovannucci@gmail.com>
2024-09-07 13:59:06 -0300

    typescript

---
+++ after/plugin/eslint.lua
@@ -0,0 +1,9 @@
+require'lspconfig'.eslint.setup({
+  --- ...
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
