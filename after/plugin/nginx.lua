44a8c32e9bad350394a19ba79548357ed8e2a56f
khayo.rodrigues <khayovannucci@gmail.com>
2024-09-09 23:24:43 -0300

    remoção de teste nginx

--- after/plugin/nginx.lua
+++
@@ -1,7 +0,0 @@
-required'lspconfig'.nginx_ls.setup({
-    cmd = { "nginx-language-server" },  -- Certifique-se de que o comando está no PATH
-    filetypes = { "nginx" },
-    root_dir = function(fname)
-      return lspconfig.util.find_git_ancestor(fname) or vim.loop.os_homedir()
-    end,
-  })
