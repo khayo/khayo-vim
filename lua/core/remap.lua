cde4a76717b6d0a8611bd2f3db545c465452b1f6
khayo.rodrigues <khayovannucci@gmail.com>
2025-07-26 19:58:18 -0400

    teclas lsp

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -61,3 +61,19 @@ vim.keymap.set('n', '<leader>vh' ,"<cmd>split<CR>")
 -- abre os gerenciadores
 vim.keymap.set('n', '<leader>M', "<cmd>Mason<CR>")
 vim.keymap.set('n', '<leader>L', "<cmd>Lazy<CR>")
+
+-- LSP
+vim.keymap.set({'n', 'x'}, '<F13>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+vim.keymap.set('n', '<F14>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
