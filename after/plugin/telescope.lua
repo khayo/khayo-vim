3e49616859e41ec52c61fd5d6c06b75b2960d087
khayo.rodrigues <khayovannucci@gmail.com>
2025-08-12 03:47:16 -0400

    busca por atalhos de tecla

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,3 +10,34 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 -- vim.keymap.set('n', '<leader>fs', function()
 -- 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
 -- end)
+
+local telescope = require('telescope')
+
+-- Função para listar apenas atalhos customizados
+local function my_keymaps()
+  builtin.keymaps({
+    -- Mostra todos e depois filtra via previewer
+    attach_mappings = function(prompt_bufnr, map)
+      local actions = require('telescope.actions')
+      local action_state = require('telescope.actions.state')
+
+      -- Pressione <CR> para aplicar o filtro só com <leader>
+      map('i', '<CR>', function()
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        picker:refresh(
+          require('telescope.finders').new_table {
+            results = vim.tbl_filter(function(entry)
+              return entry.lhs:find("<leader>") ~= nil
+            end, picker.finder.results)
+          },
+          { reset_prompt = true }
+        )
+      end)
+
+      return true
+    end
+  })
+end
+
+-- Atalho para abrir lista filtrada
+vim.keymap.set('n', '<leader>km', my_keymaps, { desc = "Meus atalhos (<leader>)" })
