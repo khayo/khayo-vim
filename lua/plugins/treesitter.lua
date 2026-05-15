486176dbc25237730ec4be9345ef36c5b845f2ca
khayo.rodrigues <khayovannucci@gmail.com>
2026-05-15 00:21:57 -0400

    treesetter
    I'm not sure why this config exists, but it's working on mac

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,22 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     version = false,
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
+        "nvim-treesitter/nvim-treesitter-textobjects", lazy = false
     },
     -- {
     --     'nvim-treesitter/playground'
     -- },
+    -- config = function()
+    --     require("nvim-treesitter.configs").setup({
     config = function()
-        require("nvim-treesitter.configs").setup({
+      local ok, configs = pcall(require, "nvim-treesitter.configs")
+      if not ok then return end
+
+      configs.setup({
             sync_install = false,
             ignore_install = { "javascript" },
             modules = {},
