1038ef58fb4b3e29952eef89fb8b918c94eed393
khayo.rodrigues <khayovannucci@gmail.com>
2026-06-27 14:49:11 -0400

    new sitax

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,7 +15,7 @@ return {
     },
     {
         'rose-pine/neovim',
-        as = 'rose-pine',
+        name = 'rose-pine',
         config = function()
            vim.cmd('colorscheme rose-pine')
         end
